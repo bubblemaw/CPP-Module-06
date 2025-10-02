ceacda01d9a85bedf76b3bc0473d633f7ceb9544
Maw <mawuli.asase@gmail.com>
2025-10-02 14:43:17 +0200

    working on ex01

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maw <maw@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/01 18:10:03 by maw               #+#    #+#             */
+/*   Updated: 2025/10/01 22:36:54 by maw              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_HPP
+# define SCALARCONVERTER_HPP
+# include <string>
+# include <iostream>
+# include <ostream>
+
+class ScalarConverter
+{
+	private:
+	public:
+		static void convert(std::string literal);
+
+};
+
+
+#endif
